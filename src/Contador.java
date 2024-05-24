5e3b96c5f954f65c2c8462190e803870b4817cf3
juliana quachio <166079880+julianaquachio@users.noreply.github.com>
2024-05-24 18:46:40 -0300

    contador

---
+++ src/Contador.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o primeiro parâmetro: ");
+		int parametroUm = sc.nextInt();
+
+}
+    private Scanner sc;
+		System.out.print("Digite o segundo parâmetro: ");
+		int parametroDois = sc.nextInt();
+
+		try {
+			int parametroUm;
+            contar(parametroUm, parametroDois);
+		} catch (InvalidParametersException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	static void contar(int parametroUm, int parametroDois ) throws InvalidParametersException {
+		//Validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if(parametroUm>parametroDois) {
+			throw new InvalidParametersException("O segundo parâmetro deve ser maior que o primeiro.");
+		}
+
+		//Imprimir os números com base na variável contagem
+		int contagem = parametroDois - parametroUm;
+		for(int i=1; i <= contagem; i++) {
+			System.out.println("Imprimindo o número " + i);
+		}
+	}
---
+++ src/InvalidParametersException.java
@@ -0,0 +1,13 @@
+
+public class InvalidParametersException {
+
+    public InvalidParametersException(String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public char[] getMessage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMessage'");
+    }
+
+}
